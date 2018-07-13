76a81f0ee81cdbf1bcfe72fd99818333dfed67fa
Braden Jageman <bjageman@surgeforward.com>
2018-07-13 14:10:28 -0700

    workin on refactor

---
+++ Assets/Board/Checkpoints/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Robo.Bots;
+
+namespace Robo.Board{
+	[SelectionBase]
+	public class Checkpoint : Waypoint, IObstacle {
+
+		BotMovement bot;
+
+		public string GetObstacleName(){
+			return "Checkpoint";
+		}
+
+		public void endTurnTrigger(BotMovement bot){
+			bot.GetComponent<Score>().CollectCheckpoint(this);
+			TurnManager turnManager = FindObjectOfType<TurnManager>();
+			turnManager.AddPlayerToQueue(bot);
+		}
+	}
+}
---
+++ Assets/GridPositionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Robo.Board;
+
+public class GridPositionHandler : MonoBehaviour {
+	
+	[SerializeField] [Range(.01f, .5f)] public float waypointThreshold = .1f; 
+
+	Waypoint currentWaypoint;
+	BoardProcessor board;
+
+	// Use this for initialization
+	void Start () {
+		board = FindObjectOfType<BoardProcessor>();
+		currentWaypoint = board.GetNearestWaypoint(transform.position.x, transform.position.z, waypointThreshold);
+		transform.position = currentWaypoint.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
